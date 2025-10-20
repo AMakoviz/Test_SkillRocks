de110db8afc7cb5eaf6c61ca77b26a44c3457aed
Makoviz Anastasia <chanas98@gmail.com>
2025-10-20 23:51:03 +0300

    1833 add find user by id

--- SkillRocks_test/src/main/java/com/example/SkillRocks_test/repository/UserRepository.java
+++ SkillRocks_test/src/main/java/com/example/SkillRocks_test/repository/UserRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+    List<UserEntity> findByFio(String username);
 
     Optional<UserEntity> findUserEntityById(UUID id);
 }
